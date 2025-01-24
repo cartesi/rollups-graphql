05d04544d17862adfa46bbcd3985d55be7cabb82
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-01-24 13:41:01 -0300

    fix: fix contract

--- pkg/convenience/synchronizer_node/syncronizer_test.go
+++ pkg/convenience/synchronizer_node/syncronizer_test.go
@@ -2,4 +2,4 @@ package synchronizernode
 
 const RAW_DB_URL = "postgres://postgres:password@localhost:5432/rollupsdb?sslmode=disable"
 const TOTAL_INPUT_TEST = 100
-const DEFAULT_TEST_APP_CONTRACT = "0xc812734eb42e12611CD2497569c451baD0f50A2d"
+const DEFAULT_TEST_APP_CONTRACT = "0x3838373036343633393563643237636430653665"
