578442b332fb6cef727890bfa4516e6d96a3a1ee
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2025-04-17 15:39:38 -0300

    ci: update to alpha 4

--- ci/scripts/send_inputs.sh
+++ ci/scripts/send_inputs.sh
@@ -3,7 +3,7 @@
 # Global variables
 INPUT_BOX_ADDRESS=0xB6b39Fb3dD926A9e3FBc7A129540eEbeA3016a6c
 APPLICATION_ADDRESS=0x8e3c7bF65833ccb1755dAB530Ef0405644FE6ae3
-MNEMONIC="test test test test test test test test test test test junk"
+PRIVATE_KEY="0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
 RPC_URL="http://localhost:8545"
 
 # Loop for execute the command 101 times with sequential hexadecimal INPUT
@@ -14,8 +14,10 @@ for ((i=0; i<101; i++)); do
     # Execute the command in line
     echo "Running the command $((i+1)) of 101 with INPUT=$INPUT..."
     cast send \
-        --mnemonic "$MNEMONIC" \
+        --private-key "$PRIVATE_KEY" \
         --rpc-url "$RPC_URL" \
+        --nonce $i\
+        --async \
         $INPUT_BOX_ADDRESS "addInput(address,bytes)(bytes32)" $APPLICATION_ADDRESS $INPUT
 
     # Verify if the last command was successful
@@ -23,7 +25,6 @@ for ((i=0; i<101; i++)); do
         echo "Error executing the command $((i+1)). Stopping the script."
         exit 1
     fi
-    sleep 1
 done
-
+sleep 10
 echo "All commands were executed successfully!"
